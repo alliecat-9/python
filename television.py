8d9bce4655395fc06705422640a4f89d6d83b096
Allie <allisonmckee@unomaha.edu>
2025-11-17 10:13:35 -0600

    Add type hints to Television class methods and attributes
    Added type hints to class attributes and methods.

--- television.py
+++ television.py
@@ -1,49 +1,77 @@
 class Television:
-    MIN_VOLUME = 0
-    MAX_VOLUME = 2
-    MIN_CHANNEL = 0
-    MAX_CHANNEL = 3
+    MIN_VOLUME: int = 0
+    MAX_VOLUME: int = 2
+    MIN_CHANNEL: int = 0
+    MAX_CHANNEL: int = 3
 
-    def __init__(self):
-        self.__status = False
-        self.__muted = False
-        self.__volume = Television.MIN_VOLUME
-        self.__channel = Television.MIN_CHANNEL
+    def __init__(self) -> None:
+        """
+        Set the default values for status, muted, volume, and channel.
+        """
+        self.__status: bool = False
+        self.__muted: bool = False
+        self.__volume: int = Television.MIN_VOLUME
+        self.__channel: int = Television.MIN_CHANNEL
 
-    def power(self):
+    def power(self) -> None:
+        """
+        Turn the television on or off by switching the status value.
+        """
         self.__status = not self.__status
 
-    def mute(self):
+    def mute(self) -> None:
+        """
+        Mute or unmute the television when it is on.
+        """
         if self.__status:
             self.__muted = not self.__muted
 
-    def channel_up(self):
+    def channel_up(self) -> None:
+        """
+        Increase the channel when the television is on.
+        Wrap to the minimum channel if the maximum is passed.
+        """
         if self.__status:
             if self.__channel == Television.MAX_CHANNEL:
                 self.__channel = Television.MIN_CHANNEL
             else:
                 self.__channel += 1
 
-    def channel_down(self):
+    def channel_down(self) -> None:
+        """
+        Decrease the channel when the television is on.
+        Wrap to the maximum channel if the minimum is passed.
+        """
         if self.__status:
             if self.__channel == Television.MIN_CHANNEL:
                 self.__channel = Television.MAX_CHANNEL
             else:
                 self.__channel -= 1
 
-    def volume_up(self):
+    def volume_up(self) -> None:
+        """
+        Increase the volume when the television is on.
+        Unmute automatically if muted.
+        """
         if self.__status:
             if self.__muted:
                 self.__muted = False
             if self.__volume < Television.MAX_VOLUME:
                 self.__volume += 1
 
-    def volume_down(self):
+    def volume_down(self) -> None:
+        """
+        Decrease the volume when the television is on.
+        Unmute automatically if muted.
+        """
         if self.__status:
             if self.__muted:
                 self.__muted = False
             if self.__volume > Television.MIN_VOLUME:
                 self.__volume -= 1
 
-    def __str__(self):
+    def __str__(self) -> str:
+        """
+        Return the television details in the required format.
+        """
         return f"Power = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}"
